003fe76fa3ac63e7aa210353372b1d1dfaea2851
Vitalii Znak <vitaliiznak@users.noreply.github.com>
2025-02-05 02:56:33 +0100

    CORS update

--- api/app/main.py
+++ api/app/main.py
@@ -14,13 +14,17 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:4201",
-        "http://localhost:4200",
-        "http://127.0.0.1:4201",
         "http://127.0.0.1:4200",
+        "http://127.0.0.1:4201",
+        "http://127.0.0.1:5173",
+        "http://127.0.0.1:8000",
+        
+        "http://localhost:4200",
+        "http://localhost:4201",
         "http://localhost:5173",
+        "http://localhost:8000",
         "http://api:8000",
-        "http://localhost:8000"
+ 
     ],
     allow_credentials=True,
     allow_methods=["*"],
